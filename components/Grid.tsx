073a7006b8b1f07160e2833e47e13be3845aae58
Itay Oshri <itay.oshri@gmail.com>
2024-02-05 20:57:59 +0200

    INIT: Submit function

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -4,11 +4,26 @@ import { useSnapshot } from 'valtio'
 import { state } from '../pages'
 import Row from './Row'
 
+const Submit = () => {
+  for (const w of state.words) {
+    if (w.checked) {
+      const group = state.groups[w.group]
+      for (const word of group.correctWords) {
+        if (!state.words[word].checked) {
+          return false
+        }
+      }
+      return true
+    }
+  }
+  return false
+}
+
 export default function Grid() {
   const snap = useSnapshot(state)
   return (
     <div className="flex flex-col gap-2">
-      {snap.words.map((row, id) => (
+      {snap.grid.map((row, id) => (
         <Row arr={row} row={id} key={id} />
       ))}
     </div>
