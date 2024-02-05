799634eb3f1b85b0bfe732b5db494beefb873bfa
Itay Oshri <itay.oshri@gmail.com>
2024-02-05 16:28:27 +0200

    FEATURE: Word Card

--- components/WordCard.tsx
+++ components/WordCard.tsx
@@ -1,18 +1,28 @@
-import { Dispatch, SetStateAction } from 'react'
+import { state } from '../pages'
+import { IWord } from '../interfaces'
 
-interface WordCardProps {
-  word: string
-  checked: boolean
-  setChecked: Dispatch<SetStateAction<boolean>>
+const SetChecked = (row, id) => {
+  state.words[row][id].checked = !state.words[row][id].checked
 }
 
-export default function WordCard({ word, checked, setChecked }: WordCardProps) {
+interface WordCardProps extends IWord {
+  rowId: number
+  id: number
+}
+
+export default function WordCard({
+  word,
+  checked,
+  completed,
+  rowId,
+  id,
+}: WordCardProps) {
   return (
     <div
       className={`flex bg-stone-200 w-1/4 justify-center items-center h-[22.5vw] /h-24 rounded-lg text-black font-bold ${
         checked ? 'bg-stone-600 text-white' : ''
       }`}
-      onClick={() => setChecked((prev) => !prev)}
+      onClick={() => SetChecked(rowId, id)}
     >
       {word}
     </div>
---
+++ interfaces/index.ts
@@ -0,0 +1,7 @@
+type Row = 0 | 1 | 2 | 3
+
+export interface IWord {
+  word: string
+  checked: boolean
+  completed: boolean
+}
