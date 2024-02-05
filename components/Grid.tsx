37909035dbd4b4bcf5483deac2512c797c7b4b7c
Itay Oshri <itay.oshri@gmail.com>
2024-02-05 23:55:40 +0200

    FEATURE: Checking

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,26 +1,10 @@
-import { useState } from 'react'
-import WordCard from './WordCard'
 import { useSnapshot } from 'valtio'
 import { state } from '../pages'
 import Row from './Row'
 
-const Submit = () => {
-  for (const w of state.words) {
-    if (w.checked) {
-      const group = state.groups[w.group]
-      for (const word of group.correctWords) {
-        if (!state.words[word].checked) {
-          return false
-        }
-      }
-      return true
-    }
-  }
-  return false
-}
-
 export default function Grid() {
   const snap = useSnapshot(state)
+
   return (
     <div className="flex flex-col gap-2">
       {snap.grid.map((row, id) => (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,69 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import WordCard from '../components/WordCard'
-import { useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import Grid from '../components/Grid'
-import { proxy } from 'valtio'
+import { proxy, snapshot, useSnapshot } from 'valtio'
 import { IGroup, IGridWord } from '../interfaces'
+import Group from '../components/Group'
+import Button from '../components/Button'
 const inter = Inter({ subsets: ['latin'] })
+import axios from 'axios'
 
 export const state = proxy({
-  grid: [[{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }]] as IGridWord[][],
-  groups: [
-    {
-      correctWords: [0, 1, 2, 3],
-      completed: true,
-    },
-  ] as IGroup[],
-  words: [
-    { word: 'Example', group: 0, checked: false },
-    { word: 'hey', group: 0, checked: false },
-    { word: 'Good bye', group: 0, checked: false },
-    { word: 'Good morning', group: 0, checked: false },
-  ],
+  grid: [] as IGridWord[][],
+  groups: [] as IGroup[],
+  words: [],
 })
 
 export default function Home() {
-  return (
+  const resetGrid = useCallback(() => {
+    state.grid = []
+    var arr = []
+    var counter = 0
+    for (const w in state.words) {
+      if (state.words[w].completed) continue
+      counter++
+      arr.push({ id: w })
+      if (counter != 0 && counter % 4 == 0) {
+        counter = 0
+        state.grid.push(arr)
+        arr = []
+      }
+    }
+  }, [])
+
+  const Submit = useCallback(() => {
+    for (const w of state.words) {
+      if (w.checked) {
+        const group = state.groups[w.group]
+        for (const word of group.correctWords) {
+          if (!state.words[word].checked) {
+            return false
+          }
+        }
+        return true
+      }
+    }
+    return false
+  }, [])
+
+  useEffect(() => {
+    axios('/api/hello').then((res) => {
+      console.log(res.data)
+      state.groups = res.data.groups
+      state.words = res.data.words
+      resetGrid()
+    })
+  }, [resetGrid])
+
+  const snap = useSnapshot(state)
+
+  return snap.groups.length > 0 ? (
     <div className="bg-white h-screen w-screen">
       <Grid />
+      <Group {...state.groups[0]} />
+      <Button onClick={() => console.log(Submit())}>Submit</Button>
     </div>
-  )
+  ) : null
 }
