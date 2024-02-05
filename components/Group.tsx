09492f60375bdb6710271c80aae0d0f4544598cb
Itay Oshri <itay.oshri@gmail.com>
2024-02-05 21:34:21 +0200

    FEATURE: Group card color

--- components/Group.tsx
+++ components/Group.tsx
@@ -2,6 +2,13 @@ import { useMemo } from 'react'
 import { IGroup } from '../interfaces'
 import { state } from '../pages'
 
+const colors = {
+  yellow: 'bg-yellow-200',
+  blue: 'bg-blue-200',
+  purple: 'bg-purple-300',
+  green: 'bg-lime-400',
+}
+
 export default function Group({ correctWords, completed, category }: IGroup) {
   const words = useMemo(() => {
     var temp = ''
@@ -13,7 +20,9 @@ export default function Group({ correctWords, completed, category }: IGroup) {
   }, [correctWords])
 
   return (
-    <div className="flex flex-col h-[22.5vw] items-center justify-center gap-1 rounded-lg bg-yellow-200">
+    <div
+      className={`flex flex-col h-[22.5vw] items-center justify-center gap-1 rounded-lg ${colors['purple']}`}
+    >
       <h1 className="text-2xl font-black">{category}</h1>
       <a>{words}</a>
     </div>
